cd4b03b1f90e351161c7aa2cc44cd08d8b6f74d9
AF <100952220+ibnbayo@users.noreply.github.com>
2022-12-09 08:26:48 +0100

    set vite config

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,8 @@ module.exports = {
         rollupOptions: {
             input: {
                 main: path.resolve(__dirname, '/index.html'),
-                customchair: path.resolve(__dirname, 'projects/custom-chairs/index.html'),
                 plane: path.resolve(__dirname, 'projects/plane/index.html'),
-                sphere: path.resolve(__dirname, 'projects/sphere/index.html'),
-                shootout: path.resolve(__dirname, 'projects/shootout/index.html'),
+
             },
         },
     },
